bb3eff5995e98292a2c75be1f9d99f9b338934b8
Alex Rook <alex.rook@gmail.com>
2019-10-06 21:14:46 -0600

    stubbed out functions

---
+++ src/pandas_helpers.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import os
+
+
+def load_df(filename):
+    '''
+    loads {filename} as a pandas DataFrame 
+    in: filename
+    out: returns pandas DataFrame.
+    '''
+    pass
+
+
+def count_rows(dataframe):
+    '''
+    in: dataframe 
+    out: returns the number of rows in that dataframe
+    '''
+    pass
+
+def count_cols(dataframe):
+    ''' 
+    in: dataframe
+    out: returns the number of columns in that dataframe
+    '''
+    pass
+
+
+def sum_two_columns(dataframe, col1, col2):
+    in: a dataframe and the name of two columns as parameters
+    out: returns the sum of those columns.
+    pass
+
+
+def get_features_and_target(da): 
+    ''' 
+    return the features X and target y from a dataframe as NumPy arrays,
+    so that we could use them in a machine learning algorithm.
+    (The target will be a single column in your dataframe, and 
+    features will be all other columns.) Think about what parameters
+    you’ll need to accept in order to accomplish this, and what you’ll
+    need to return.
+    '''
+    pass
